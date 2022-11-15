39bccbaf24505d1cbf2a312a30126d27afe77a66
Jack Bonnelycke <jbonnelycke@gmail.com>
2022-11-15 18:10:35 -0500

    added basic site structure

---
+++ Pages/NewMusicFridays.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebApp.Pages;
+
+public class NewMusicFridaysModel : PageModel
+{
+    private readonly ILogger<NewMusicFridaysModel> _logger;
+
+    public NewMusicFridaysModel(ILogger<NewMusicFridaysModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
---
+++ Pages/Portfolio.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebApp.Pages;
+
+public class PortfolioModel : PageModel
+{
+    private readonly ILogger<PortfolioModel> _logger;
+
+    public PortfolioModel(ILogger<PortfolioModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
